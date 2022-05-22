c92b1276b4271b52a71983c4e7e0a473beb1e4d2
Yasomalako <Yasomalako@gmail.com>
2022-05-22 23:59:19 +0300

    string

---
+++ main.js
@@ -0,0 +1,7 @@
+//צרו פונקציה שמחזירה מס רנדומאלי בין 0 ל10
+function printRandomnumber(randomNum){
+    var randomNumb = Math.floor(Math.random()*10);
+ 
+ return console.log(randomNumb)
+}
+printRandomnumber(randomNum)
